e73b8887bcbfaaefbe29a647204528cf21c3d2a4
Tim Oltman <toltman@gmail.com>
2020-05-09 09:04:08 -0400

    added password encryption and user login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 "use strict";
 const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
 
 module.exports = (sequelize) => {
   class User extends Sequelize.Model {}
@@ -9,20 +10,31 @@ module.exports = (sequelize) => {
       email: Sequelize.STRING,
       password: Sequelize.STRING,
     },
-    { sequelize }
+    {
+      hooks: {
+        beforeCreate: async function (user) {
+          user.password = await bcrypt.hash(user.password, 10);
+        },
+      },
+
+      sequelize,
+    }
   );
   User.authenticate = async function (email, password, callback) {
     try {
+      // look up user by email
       const user = await User.findOne({ where: { email: email } });
       if (!user) {
+        // user not found
         const err = new Error("User not found.");
         err.status = 401;
         return callback(err);
       }
 
       // compare password
-      // needs to be updated with bcrypt
-      if (password === user.password) {
+      const result = await bcrypt.compare(password, user.password);
+      console.log(result);
+      if (result) {
         return callback(null, user);
       } else {
         return callback();
